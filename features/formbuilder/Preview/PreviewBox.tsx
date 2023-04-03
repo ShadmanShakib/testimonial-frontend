aa66604b0f20564b7f311f40ea01cfa3ffa692d0
Shadman Shakib <sm.shakib007@gmail.com>
2023-04-03 16:51:45 +0600

    preveiw card updated

--- features/formbuilder/Preview/PreviewBox.tsx
+++ features/formbuilder/Preview/PreviewBox.tsx
@@ -7,9 +7,9 @@ export default function PreviewBox() {
     <div className="flex h-full flex-col items-center overflow-y-scroll px-8 pt-8 pb-12">
       <div
         style={{ backgroundColor: `${backgroundColor}` }}
-        className="flex h-[90%] w-full flex-none flex-col overflow-hidden rounded-md shadow-xl  ring-4 ring-gray-800"
+        className="flex h-[90%] w-full flex-none flex-col  overflow-hidden rounded-md shadow-xl  ring-4 ring-gray-800"
       >
-        <div className="flex min-h-screen flex-col items-center justify-center px-4 pt-12 pb-8">
+        <div className="flex h-full flex-col justify-center  px-4 pt-12 pb-8">
           <PreviewCard />
         </div>
       </div>
