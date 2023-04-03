a0e9a3172451944f14e4f9f0cb4a7d2fed6827a8
Shadman Shakib <sm.shakib007@gmail.com>
2023-04-03 23:30:37 +0600

    Form context

--- features/formbuilder/Preview/PreviewCard.tsx
+++ features/formbuilder/Preview/PreviewCard.tsx
@@ -1,8 +1,7 @@
-import { VideoSvg, PenSvg } from "@/components/icons";
 import React from "react";
-import { useTestimonidal } from "@/lib/context/Testimonial";
-import { useFormContext } from "@/lib/context";
 import Image from "next/image";
+import { VideoSvg, PenSvg } from "@/components/icons";
+import { useFormContext } from "@/lib/context";
 function PreviewCard() {
   const { primaryColor, logo, title, message } = useFormContext();
 
