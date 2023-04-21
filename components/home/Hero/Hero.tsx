4171c3488c121d808d50b5c39695aa8d332a13ae
Shadman Shakib <sm.shakib007@gmail.com>
2023-04-21 16:20:23 +0600

    Homepage update

--- components/home/Hero/Hero.tsx
+++ components/home/Hero/Hero.tsx
@@ -2,11 +2,14 @@ import React from "react";
 
 function Hero() {
   return (
-    <div className="container relative pt-32 ">
-      <div className="max-w-4x mx-auto text-center">
-        <span>Testimonidals Made easy</span>
-        <h1 className="text-3xl leading-normal md:text-5xl md:leading-snug lg:leading-snug">
-          Collecting testimonials doesnt get easier than this
+    <div className="container relative mx-auto max-w-screen-xl pt-32 ">
+      <div className="max-w-4x mx-auto text-center ">
+        <span className="text-lg  text-brand">
+          The easiest way to showcase happy customers.
+        </span>
+        <h1 className="text-3xl font-semibold leading-normal md:text-5xl md:leading-snug lg:leading-snug">
+          TestiWizard makes collecting and organizing testimonials easier than
+          ever before
         </h1>
         <div className="mx-auto mt-4 max-w-2xl text-gray-700 sm:text-lg md:text-xl md:leading-normal">
           <p>
