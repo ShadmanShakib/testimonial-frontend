d9684918deca19f757ecd55a8edc8dccf500e946
Shadman Shakib <sm.shakib007@gmail.com>
2023-04-29 16:11:36 +0600

    useGetAllForms

--- pages/forms/index.tsx
+++ pages/forms/index.tsx
@@ -2,22 +2,22 @@ import React from "react";
 import { Sidebar } from "@/components/common";
 import { FormList } from "@/features/formbuilder";
 import { CreateFormDialog, FormHeader } from "@/features/dashboard/component";
-import { getUserForms } from "@/utils/services/form";
+import { useGetAllFroms } from "@/features/formbuilder/services";
 import { useUiContext } from "@/lib/context";
+import { useMutation } from "react-query";
+
 import _ from "underscore";
 function Forms() {
-  //fetch data
-  const [forms, setForms] = React.useState([]);
-  React.useEffect(() => {
-    getUserForms().then((res) => {
-      setForms(res);
-    });
-  }, []);
+  //services to fetch data
+  const { isLoading, error, data } = useGetAllFroms();
+
+  //Ui context
   const { isCreateFormDialog, openCreateFormDialog, closeCreateFormDialog } =
     useUiContext();
   const setIsCreateFormDialog = () => {
     isCreateFormDialog ? closeCreateFormDialog() : openCreateFormDialog();
   };
+
   return (
     <main className="relative flex">
       <Sidebar />
@@ -25,12 +25,18 @@ function Forms() {
         <div className="relative px-6">
           <FormHeader onCreateNew={openCreateFormDialog} />
         </div>
-        <div className="mt-4 flex flex-col gap-1">
-          {forms.length > 0 &&
-            _.map(forms, (form: any) => {
-              return <FormList key={form._id} title={form.values.title} />;
-            })}
-        </div>
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div>Something went wrong</div>
+        ) : (
+          <div className="mt-4 flex flex-col gap-1">
+            {data.length > 0 &&
+              _.map(data, (form: any) => {
+                return <FormList key={form._id} title={form.values.title} />;
+              })}
+          </div>
+        )}
       </div>
       <CreateFormDialog
         open={isCreateFormDialog}
