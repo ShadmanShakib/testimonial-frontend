942baaa2efa043e70738c878ae177687746def88
Shadman Shakib <sm.shakib007@gmail.com>
2023-05-11 06:40:31 +0600

    Link highlight

--- features/dashboard/components/Sidebar/SidebarButton.tsx
+++ features/dashboard/components/Sidebar/SidebarButton.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import cn from "clsx";
 
 type Props = {
   icon?: React.ReactNode;
@@ -11,10 +12,14 @@ type Props = {
 const SidebarButton = (props: Props) => {
   const { icon, name, href } = props;
   const pathname = usePathname();
-  const isActive = pathname.startsWith(href);
+  const isActive = pathname.endsWith(href);
+  const cln = cn(
+    "relative w-full flex mb-[1px] items-center gap-3 rounded-md px-2 py-[5px] text-left text-sm font-medium text-zinc-600 duration-100 hover:bg-zinc-200/70",
+    isActive && "bg-zinc-200/70"
+  );
   return (
     <Link className="w-full" href={href}>
-      <button className="relative w-full flex mb-[1px] items-center gap-3 rounded-md px-2 py-[5px] text-left text-sm font-medium text-zinc-600 duration-100 hover:bg-zinc-200/70 ">
+      <button className={cln}>
         <div>{icon}</div>
         <div>{name}</div>
         <div className="flex-grow"></div>
